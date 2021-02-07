92903e5a11fbd01b1984fce48721e73badfd7d6e
Paige Vannelli <paigevannelli@gmail.com>
2021-02-07 11:37:21 -0700

    Add pseudocode to recipe repo

--- src/recipe-repo.js
+++ src/recipe-repo.js
@@ -1,1 +1,6 @@
-//holds an array of our recipes 
+const RecipeRepo = {
+    //takes in recipe data 
+    //holds an array of our recipes 
+    //filters recipe based on name or ingredient helper function 
+    //filters based on tags helper function 
+}
