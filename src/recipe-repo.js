5a473aa98bd8841e272cb593f80d94907413f1ad
Paige Vannelli <paigevannelli@gmail.com>
2021-02-09 16:28:36 -0700

    Create filterByIngredient method

--- src/recipe-repo.js
+++ src/recipe-repo.js
@@ -3,19 +3,34 @@ class RecipeRepo {
         this.recipes = recipes;
     }
 
-    filterByIngredient(ingredient) {
-        this.filter(ingredient)
-        // filter through all recipes  
+    filterByIngredient(ingredientName) {
+        const recipesWithIngredient = this.recipes.filter(recipe => {
+            return recipe.ingredients.some(ingredient => ingredient.name.includes(ingredientName))
+        })
+        return recipesWithIngredient;
+
+        // iterate through the recipes
+        // I want to return a filtered recipes
+        // inside the loop for each recipe I want to iterate through each ingredient
+        // if ingredient.name === ingredient when that's true I want to push recipe
     }
 
     filterByTag(tags) {
         const recipesWithTag = this.recipes.filter(recipe => {
-            for (let i = 0; i < tags.length; i++) {
-                return recipe.tags.includes(tags[0]) || recipe.tags.includes(tags[1]);
-            }
+            return tags.every(tag => {
+                return recipe.tags.includes(tag)
+            })
+            // for (let i = 0; i < tags.length; i++) {
+            //     return recipe.tags.includes(tags[0]) || recipe.tags.includes(tags[1]);
+            // }
         });
+        console.log(recipesWithTag)
         return recipesWithTag;
-      
+    // given array of tags filter all the include tags without repeats
+    // for each recipe if it includes all of the tags filter it
+    // for each tag in tags, does recipe.tags include it
+    
+    // tagsargument.includes(each tag in recipe)
         // filter through all of the recipes
         // use .includes to check if recipes.tags.includes(tag)
         // .filter should returna filtered down array of recipes that meet the criterea 
--- test/recipe-repo-test.js
+++ test/recipe-repo-test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const RecipeRepo = require('../src/recipe-repo');
 const {recipeTestData} = require('../data/test-data');
+const Recipe = require('../src/recipe');
 const expect = chai.expect;
 
 describe('RecipeRepo', function() {
@@ -19,10 +20,18 @@ describe('RecipeRepo', function() {
         expect(recipeTestData).to.be.an('array')
     })
 
-    it(`should be able to filter by one tag and return an array of recipes`, function() {
-        const recipeRepo = new RecipeRepo(recipeTestData);
-        // console.log(recipeTestData)
-        recipeRepo.filterByTag(['side dish', 'starter'])
+    it(`should be able to search by an ingredient and return an array of recipes`, function() {
+        const recipe1 = new Recipe(recipeTestData[0]);
+        const recipe2 = new Recipe(recipeTestData[1]);
+        const recipe3 = new Recipe(recipeTestData[2]);
+        
+        let recipeTest = []
+        recipeTest.push(recipe1)
+        recipeTest.push(recipe2)
+        recipeTest.push(recipe3)
+        // console.log(recipeTest[2].ingredients)
+        const recipeRepo = new RecipeRepo(recipeTest);
+        recipeRepo.filterByIngredient('salt')
         // expect(recipeRepo.filterByTag('lunch')).to.be.equal()
     })
 
