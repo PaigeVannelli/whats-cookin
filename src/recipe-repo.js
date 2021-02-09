a4bcff99fb1b2321a6434cdb4ec0f2997b22e5c5
Paige Vannelli <paigevannelli@gmail.com>
2021-02-09 14:54:47 -0700

    Create testing setup for Recipe repo

--- src/recipe-repo.js
+++ src/recipe-repo.js
@@ -1,6 +1,19 @@
-const RecipeRepo = {
-    //takes in recipe data 
-    //holds an array of our recipes 
-    //filters recipe based on name or ingredient helper function 
-    //filters based on tags helper function 
+class RecipeRepo {
+    constructor(recipes) {
+        this.recipes = recipes;
+    }
+
+    filterByIngredient(ingredient) {
+        this.filter(ingredient)
+        // filter through 
+    }
+
+    filterByTag(tag) {
+        this.filter(tag)
+        // filter through all of the recipes
+        // use .includes to check if recipes.tags.includes(tag)
+        // .filter should returna filtered down array of recipes that meet the criterea 
+    }
 }
+
+module.exports = RecipeRepo;
--- test/recipe-repo-test.js
+++ test/recipe-repo-test.js
@@ -1,4 +1,22 @@
 const chai = require('chai');
-const recipeRepo = require('../src/recipe-repo');
-// const testData = require('../src/test-data');
+const RecipeRepo = require('../src/recipe-repo');
+const {recipeTestData} = require('../data/test-data');
 const expect = chai.expect;
+
+describe('RecipeRepo', function() {
+
+    it(`Should be a function`, function() {
+        expect(RecipeRepo).to.be.a(`function`);
+    });
+
+    it(`should be an instance of Ingredient`, function() {
+        const recipeRepo = new RecipeRepo(recipeTestData);
+        expect(recipeRepo).to.be.an.instanceOf(RecipeRepo);
+    });
+
+    it(`should take in an array of ingredients as an argument`, function() {
+        const recipeRepo = new RecipeRepo(recipeTestData);
+        expect(recipeTestData).to.be.an('array')
+    })
+
+});
